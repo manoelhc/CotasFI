651e322bc54ce7b29685654b2681e7816334404b
Manoel Carvalho <manoelhc@gmail.com>
2020-08-06 23:06:36 +0100

    Update gen script

--- gen.sh
+++ gen.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
 curr_date=$(date +%Y-%m-%d)
 wget http://dados.cvm.gov.br/dados/FI/DOC/INF_DIARIO/DADOS/inf_diario_fi_202007.csv -O data.csv
-git checkout -b ${curr_date}
 python3 ./parser.py data.csv
-git commit data -m "Add ${curr_date} prices"
+git tag -a ${curr_date} -m "Updated values: ${curr_date}"
 git push
