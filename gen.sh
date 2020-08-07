1d2136d35090a1efb5848509c7ac13f7eda09a4d
Manoel Carvalho <manoelhc@gmail.com>
2020-08-07 09:38:50 +0100

    quick fix

--- gen.sh
+++ gen.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 curr_date=$(date +%Y-%m-%d)
 curr_file="$(date +%Y%m)"
-wget "http://dados.cvm.gov.br/dados/FI/DOC/INF_DIARIO/DADOS/inf_diario_fi_{$curr_file}.csv" -O data.csv
+wget "http://dados.cvm.gov.br/dados/FI/DOC/INF_DIARIO/DADOS/inf_diario_fi_${curr_file}.csv" -O data.csv
 python3 ./parser.py data.csv
 git add data
 git tag -a ${curr_date} -m "Updated values: ${curr_date}"
