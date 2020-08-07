e274670d8b916c2e81ba7dedfa6a4da3d89872a4
Manoel Carvalho <manoelhc@gmail.com>
2020-08-07 09:18:08 +0100

    quick update

--- gen.sh
+++ gen.sh
@@ -1,6 +1,9 @@
 #!/bin/bash
 curr_date=$(date +%Y-%m-%d)
-wget http://dados.cvm.gov.br/dados/FI/DOC/INF_DIARIO/DADOS/inf_diario_fi_202007.csv -O data.csv
+curr_file="$(date +%Y%m)"
+wget "http://dados.cvm.gov.br/dados/FI/DOC/INF_DIARIO/DADOS/inf_diario_fi_{$curr_file}.csv" -O data.csv
 python3 ./parser.py data.csv
+git add data
 git tag -a ${curr_date} -m "Updated values: ${curr_date}"
+git commit data -m "Updated values: ${curr_date}"
 git push
